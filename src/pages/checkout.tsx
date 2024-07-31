e0e30372ff6951db201372b250a1702a30e51e81
ZeeFuzooly <ziranfuzooly@gmail.com>
2024-07-31 20:14:55 +0530

    fixing errors

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -3,10 +3,10 @@ import CheckoutForm from '../components/CheckoutForm';
 
 const Page = () => {
   // Get cart items from the store
-  const items = useCartStore((state) => state.items);
+  const items = useCartStore((state: { items: any; }) => state.items);
 
   // Calculate total price
-  const totalPrice = items.reduce((total, item) => total + item.book.price * item.quantity, 0);
+  const totalPrice = items.reduce((total: number, item: { book: { price: number; }; quantity: number; }) => total + item.book.price * item.quantity, 0);
 
   return <CheckoutForm totalPrice={totalPrice} />;
 };
